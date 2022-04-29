42d6298140aa8abf86492561adfde31100c1f2db
Timo1024 <47331071+Timo1024@users.noreply.github.com>
2022-04-29 21:22:47 +0200

    todo hinzugefügt

--- assignment_01/exercise_01.py
+++ assignment_01/exercise_01.py
@@ -228,6 +228,7 @@ def main():
     The main function should contain all functions that solved the stated tasks. 
     '''
     # T2.a
+    # TODO schauen, was wirklich in die main Funktion rein soll (vermutlich nur der Teil bei a))
 
     new_file_name = "MultipleSeqsReverse.fasta"
     file1 = args.file_one
